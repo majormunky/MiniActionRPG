73948a3c7ab0c21c32536d065c1986c8fa5c4ada
Josh Bright <josh.bright@gmail.com>
2021-07-31 09:34:59 -0700

    Instead of using an item list, we now just use a vbox container so we can have hp bars and player names

--- scenes/BattleScene.gd
+++ scenes/BattleScene.gd
@@ -15,9 +15,7 @@ var enemy_hp = 80
 var enemy_name = "Bird"
 var player_name = "Torin"
 
-onready var player_list = $MarginContainer/Panel/VBoxContainer/InfoArea/FightInfo/HBoxContainer/PlayersList
 onready var enemy_list = $MarginContainer/Panel/VBoxContainer/InfoArea/FightInfo/HBoxContainer/EnemiesList
 
 func _ready():
-	player_list.add_item(player_name)
 	enemy_list.add_item(enemy_name)
